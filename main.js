b92b68abb72ae5fe7508c724c2633bf807bd2e26
Daniel Prilik <danielprilik@gmail.com>
2015-07-27 23:55:25 -0400

    Inital Commit of 1212!
    It's a 1010! clone. Duh.

---
+++ main.js
@@ -0,0 +1,56 @@
+var size = {
+    chunk: 46,
+    chunkS: 32.5,
+    shape: {
+        ln2:[2,1],
+        ln3:[3,1],
+        ln4:[4,1],
+        ln5:[5,1],
+        sBlock:[1,1],
+        mBlock:[2,2],
+        lBlock:[3,3],
+        smallL:[2,2],
+        bigL:[3,3]
+    }
+}
+
+function changePieceSize(piece, growShrink) {
+    var shapeType = $(piece).attr("class").split(' ')[1];
+    var newSize = (growShrink=='shrink') ? size.chunkS : size.chunk;
+    
+    
+    $(piece).width(newSize*size.shape[shapeType][0]+(size.shape[shapeType][0]-1)*2)
+    $(piece).height(newSize*size.shape[shapeType][1]+(size.shape[shapeType][1]-1)*2)
+
+    $(piece).find('.chunk').height(newSize);
+    $(piece).find('.chunk').width(newSize);
+}
+
+window.onload = function () {
+    $(".piece").draggable({
+        start: function() {
+            changePieceSize(this, 'grow')
+        },
+        revert: function() {
+            // check if it was a good drop, valid position and such.
+            var allGood = false;
+            
+            if (allGood) {
+                // actually drop it
+                // delete this thing
+                return false;
+            } else {
+                // Shrink them back
+                changePieceSize(this, 'shrink')
+                
+                // Return the piece to starting position
+                $(this).data("uiDraggable").originalPosition = {
+                    top : '50%',
+                    left : 0
+                };
+                
+                return true;
+            }
+        }
+    });
+}
