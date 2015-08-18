44f83e0be878bc4071c49c6f60e8ef8e9ec807a0
Daniel Prilik <danielprilik@gmail.com>
2015-08-18 19:52:26 -0400

    tweet on game over added

--- main.js
+++ main.js
@@ -778,7 +778,7 @@ function init() {
         $(".paused").removeClass("hidden");
         $(".drag-container").draggable('disable')
     });
-    $(".high-score .tweet-button").click(function(){
+    $(".social-score").click(function(){
         var href = String.format("https://twitter.com/intent/tweet?hashtags=1212game&ref_src=twsrc%5Etfw&text=I%20just%20scored%20{0}%20points%20in%20a%20game%20called%201212!%20Can%20you%20do%20better%3F&tw_p=tweetbutton&url=prilik.ca/1212/&via=danielprilik", score)
         window.open(href);
     })
